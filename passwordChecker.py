f7dabdddc743356cab46af5e35ea80c455f8d9c5
Arya Pramod Ambekar <aryaambekar17@gmail.com>
2023-08-19 20:08:51 +0530

    moved all files and then added passwordChecker file

---
+++ passwordChecker.py
@@ -0,0 +1,4 @@
+name=input('Enter your name : ')
+password = input('Enter your password : ')
+password_encrepted = '*' * len(password) 
+print(f'helloo {name}, your password is {password_encrepted} and lenth of the password is {len(password)}' )
